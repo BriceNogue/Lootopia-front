b38e97a27f076ec496360bb5ed24f3ab2d1366f4
BriceNogue <bricenogue@gmail.com>
2025-04-15 14:34:39 +0200

    Ajout des routes

--- lootopia-front/src/app/app.routes.ts
+++ lootopia-front/src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { HuntDetailsComponent } from './pages/hunt-details/hunt-details.component';
+import { UserProfilComponent } from './pages/user-profil/user-profil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'landing',
+        component: LandingComponent,
+        data: {
+            title: 'Landing',
+            breadcrumb: 'Landing' // Pour le fil d'Ariane
+        }
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'user-profile',
+        component: UserProfilComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'hunt-details/:id',
+        component: HuntDetailsComponent
+    },
+    {
+        path: 'not-found',
+        component: NotfoundComponent
+    },
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found',
+        pathMatch: 'full'
+    }
+];
