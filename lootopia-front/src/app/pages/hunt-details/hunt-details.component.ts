e388ced9256b0fb3ed4c2292ee47e35e8b1ba953
BriceNogue <bricenogue@gmail.com>
2025-04-15 14:07:13 +0200

    Ajout de la base des pages

---
+++ lootopia-front/src/app/pages/hunt-details/hunt-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hunt-details',
+  imports: [],
+  templateUrl: './hunt-details.component.html',
+  styleUrl: './hunt-details.component.scss'
+})
+export class HuntDetailsComponent {
+
+}
