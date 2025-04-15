e3ace38f8e545d68264f1ebe5914ab4188fce4c3
BriceNogue <bricenogue@gmail.com>
2025-04-15 15:05:21 +0200

    Add spinner on app.component

--- lootopia-front/src/app/app.component.ts
+++ lootopia-front/src/app/app.component.ts
@@ -1,12 +1,58 @@
+//#region Imports
+//#region Directives
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+//#endregion Directives
+
+//#region Moduls
+import { CommonModule } from '@angular/common';
+//#endregion Moduls
+
+//#region Components
+import { HeaderComponent } from './shareds/header/header.component';
+import { FooterComponent } from './shareds/footer/footer.component';
+//#endregion Components
+//#endregion Imports
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    //#region Directives
+    RouterOutlet,
+    //#endregion Directives
+
+    //#region Moduls
+    CommonModule,
+    //#endregion Moduls
+
+    //#region Components
+    HeaderComponent,
+    FooterComponent
+    //#endregion Components
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lootopia-front';
+  loading$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private router: Router) {
+    setTimeout(() => this.loading$.next(true), 1000); // Show loading after 1 second
+    setTimeout(() => this.loading$.next(false), 3000); // Hide loading after 3 seconds
+  }
+
+  ngOnInit() {
+    this.router.navigate(['/landing']);
+  }
+
+  ngOnDestroy() {
+    
+  }
+
+  ngAfterViewInit() {
+    
+  }
 }
